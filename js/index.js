d15bc257d74f1fcd58913db79dc9abaa150d8b9f
didebbo <gianluca.napoletano.93@gmail.com>
2021-08-26 17:10:25 +0200

    aggiunta interfaccia grafica

--- js/index.js
+++ js/index.js
@@ -1,20 +1,60 @@
+
 // Dichiaro variabili ed inizializzo array cognomi
 const lastNames = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
 let userLastName, position, output;
 
+// DOM
+const ul = document.getElementById("list");
+const cognome = document.getElementById("cognome");
+const send = document.getElementById("send");
+const dom_position = document.getElementById("position");
+dom_position.style.opacity = 0;
+
+const sortList = () => {
+    for (let i = 0; i < lastNames.length; i++) {
+        let li = document.createElement("li");
+        li.innerHTML = lastNames[i];
+        ul.appendChild(li);
+    }
+}
+
+send.onclick = () => {
+    let str = cognome.value;
+    if (!isNaN(str) || str == 0) {
+        cognome.value = "";
+        return;
+    }
+
+    str = str.toLowerCase();
+    str = str[0].toUpperCase() + str.slice(1);
+    lastNames.push(str);
+    lastNames.sort();
+    ul.innerHTML = "";
+    cognome.value = "";
+    sortList();
+    if (dom_position.style.opacity == 0) dom_position.style.opacity = 1;
+    dom_position.innerHTML = "Sei alla posizione: " + parseInt((lastNames.indexOf(str) + 1));
+    ul.children[lastNames.indexOf(str)].classList.add("selected");
+
+}
+
+sortList();
+
 // Chiedo cognome all'utente e lo formatto nel seguente modo: "Cognome";
-do userLastName = prompt("Inserisci aaa il tuo cognome:").toLowerCase();
-while (!isNaN(userLastName) || userLastName.length == 0)
-userLastName = userLastName[0].toUpperCase() + userLastName.slice(1);
+// do userLastName = prompt("Inserisci aaa il tuo cognome:").toLowerCase();
+// while (!isNaN(userLastName) || userLastName.length == 0)
+// userLastName = userLastName[0].toUpperCase() + userLastName.slice(1);
 
 // Inserisco il cognome dell'utente nell'arrai ed ordino quest'ultimo in ordine alfabetico.
-lastNames.push(userLastName);
-lastNames.sort();
+// lastNames.push(userLastName);
+// lastNames.sort();
 
 // Formatto la stringa di output
-output = "Utenti:";
-for (let i = 0; i < lastNames.length; i++) output += " " + (i + 1) + ": " + lastNames[i] + ","; output = output.slice(0, -1);
-output += "\nLa tua posizione: " + (lastNames.indexOf(userLastName) + 1);
+// output = "Utenti:";
+// for (let i = 0; i < lastNames.length; i++) output += " " + (i + 1) + ": " + lastNames[i] + ","; output = output.slice(0, -1);
+// output += "\nLa tua posizione: " + (lastNames.indexOf(userLastName) + 1);
 
 // Stampo la stringa
-alert(output);
+// alert(output);
+
+
